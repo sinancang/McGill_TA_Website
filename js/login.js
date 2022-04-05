b0a494de4d9c05c880f55bdd4d9180557f343fb3
sinancang <sinancangulan@gmail.com>
2022-04-05 14:11:45 -0400

    login

--- js/login.js
+++ js/login.js
@@ -1,14 +1,11 @@
-function encrypt(message, key){ //encrypts message using key }
-
 function sendLoginRequest(){ 
 	username = document.getElementById('user').value;
-                        password = document.getElementById('pass').value;
-                        encrypted_password = encrypt(password, 5);
+        password = document.getElementById('pass').value;
+        encrypted_password = CryptoJS.AES.encrypt(password, 5);
 
-                        // make Ajax call to login.php with username & encrypted password
-                        syncRequest = new XMLHttpRequest();
-                        // TO DO: create event listener
-                        // prepare the request
-                        // then send it
-                }
-
+        // make Ajax call to login.php with username & encrypted password
+        syncRequest = new XMLHttpRequest();
+        // TO DO: create event listener
+        // prepare the request
+        // then send it
+}
---
+++ php/login.php
@@ -0,0 +1,9 @@
+<?php
+// receives username and encrypted password
+// checks the database if it finds matching user
+$username = $_POST['username']
+$password = $_POST['pass']
+
+//$rows = array_map
+
+?>
