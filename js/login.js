b8e8c1f297decf08f4879b16dada2dc60afe29fb
alara <alara@alaras-MacBook-Air-2.local>
2022-04-07 13:50:15 -0400

    fixed php route

--- js/login.js
+++ js/login.js
@@ -1,13 +1,12 @@
 function sendLoginRequest(){ 
 
-        window.alert("hi");
 	var username = document.getElementById('user').value;
         var password = document.getElementById('pass').value;
         var encrypted_password = CryptoJS.AES.encrypt(password, 5);
 
         // make Ajax call to login.php with username & encrypted password
         syncRequest = new XMLHttpRequest();
-        var url = "login.php";
+        var url = "../php/login.php";
         syncRequest.open("POST", url, true);
 
         syncRequest.addEventListener("load", function(){
