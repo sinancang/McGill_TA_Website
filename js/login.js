4b1d734ed6751aea52b9635d5ab17f7bc287457b
alaraalara <97011464+alaraalara@users.noreply.github.com>
2022-04-07 12:53:54 -0400

    Add files via upload
    Makes an AJAX call to php. created event listener prepared the request, then send it.

--- js/login.js
+++ js/login.js
@@ -1,11 +1,24 @@
 function sendLoginRequest(){ 
-	username = document.getElementById('user').value;
-        password = document.getElementById('pass').value;
-        encrypted_password = CryptoJS.AES.encrypt(password, 5);
+	var username = document.getElementById('user').value;
+        var password = document.getElementById('pass').value;
+        var encrypted_password = CryptoJS.AES.encrypt(password, 5);
 
         // make Ajax call to login.php with username & encrypted password
         syncRequest = new XMLHttpRequest();
-        // TO DO: create event listener
-        // prepare the request
-        // then send it
+        var url = "login.php";
+        syncRequest.open("POST", url, true);
+
+        syncRequest.addEventListener("load", function(){
+                if (this.responseText == 'success')
+                        alert ('successfully logged in.');
+                else
+                        alert ('failed to log in.');
+        }, false)
+
+        var fd = new FormData;
+        fd.append ('user', document.getElementById("user").value);
+        fd.append ('pass', document.getElementById("pass").value);
+        syncRequest.send (fd);
+
+
 }
