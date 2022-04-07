5fffaf606472e6822cd9c64188db9c50b0cf3284
sinancang <sinancangulan@gmail.com>
2022-04-07 13:02:09 -0400

    db and login.php

--- php/login.php
+++ php/login.php
@@ -1,9 +1,22 @@
 <?php
 // receives username and encrypted password
 // checks the database if it finds matching user
-$username = $_POST['username']
-$password = $_POST['pass']
+// $username = $_POST['username']
+// $password = $_POST['pass']
 
-//$rows = array_map
+$file = fopen("../db/db.csv","r") or die("Unable to open file!");
+$row = 1;
+while (($data = fgetcsv($file, 1000, ",")) !== FALSE){
+	
+	$num = count($data);
 
+	echo "<p> $num fields in line $row: <br /></p>/n";
+	
+	$row++;
+	for ($c = 0; $c < $num; $c++){
+		echo $data[$c] . "<br />\n"
+	}
+}
+
+fclose($file);
 ?>
