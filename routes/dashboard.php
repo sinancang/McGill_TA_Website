dbaf292bd346e6a58b23fc3eb9f67092967a6f49
David Peterman <peterman.dd@gmail.com>
2022-04-08 09:56:18 -0400

    minor project folder restructuring

---
+++ routes/dashboard.php
@@ -0,0 +1,50 @@
+<?php
+
+    //--- Routing for dashboard requests ---//
+
+    // import utility files
+    require '../utils/check_logged_in.php'; // includes function to ensure user is logged in
+
+    // check if user is logged in. if not, redirect to login page
+    if (check_logged_in($_GET['user']) === 1) {
+
+
+        // sys-ops: manage users
+        if ($_GET['view'] == 'manage-users') {
+            if (check_logged_in($_GET['user']) === 1) {
+                
+                include("../matter/manage_users.php");    
+            }
+            else {
+                echo 'fail';
+            }
+        }
+        // sys-ops: import users (csv file)
+        else if ($_GET['view'] == 'import-users') {              
+            include("../matter/import_users.php");    
+        }
+        // sys-ops: add users manually
+        else if ($_GET['view'] == 'add-manually-users') {         
+            include("../matter/add_users_manually.php");    
+        }
+        // main dashboard
+        else if ($_GET['view'] == 'main') {         
+            include("../matter/main_dashboard.php");    
+        }
+        // if no view specified, load whole dashboard because we
+        // assume it's the first time dashboard is requested
+        else if ($_GET['user'] != ""){
+            include("../matter/dashboard_default.php");
+        }
+        else {
+            echo "Page Not Found";
+        }
+    }
+    // if login check failed, redirect to login page
+    else {
+        header("Location: https://www.cs.mcgill.ca/~dpeter19/McGill_TA_Website/matter/login");
+    }
+
+    
+
+?>
