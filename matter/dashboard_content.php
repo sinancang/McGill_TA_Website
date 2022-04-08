76da820b624ab62d3e828aeef7c19e9e84c8e54c
David Peterman <peterman.dd@gmail.com>
2022-04-08 08:29:41 -0400

    small bug fix

--- matter/dashboard_content.php
+++ matter/dashboard_content.php
@@ -60,9 +60,7 @@
 
             <div id="dashboard-dynamic-content" class="dashboard-content-dynamic-content">
 
-                <div id="main-dashboard-greeting">Hi, 
-                    <div id="username" style="display: inline;"><?php echo $username ?></div>
-                    !</div>
+                <div id="main-dashboard-greeting">Hi, <?php echo $username ?></div>!</div>
 
 
                 <div class="main-dashboard-recent-activity-container">
@@ -117,7 +115,7 @@
         
 
 
-        <div id="hidden-username"><?php echo $username ?></div>
+        <div id="username"><?php echo $username ?></div>
 
         <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5eab681659f730bdd5daac20" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="../js/dashboard.js"></script>
