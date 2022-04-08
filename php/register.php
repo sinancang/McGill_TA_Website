6f1f1854eebe0ba6f90427bf7d198784eb5b22ae
Sinan Can GULAN <sgulan@teaching.CS.McGill.CA>
2022-04-07 20:19:28 -0400

    append to db.csv if user doesn
    't already exist

--- php/register.php
+++ php/register.php
@@ -14,6 +14,11 @@ while (($data = fgetcsv($file, 1000, ",")) !== FALSE){
 		exit();
 	}
 }
-
+// no other user has same username
 fclose($file);
+
+$file2 = fopen("../db/db.csv", "a")or die("Unable to open file!");
+$userData = $_POST['user'] . $_POST['pass'];
+fwrite($file2, $userData);
+fclose($file2);
 ?>
