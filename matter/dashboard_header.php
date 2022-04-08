b1a6f3bffef8d29f980d22bc9c5d8d877be5415b
David Peterman <peterman.dd@gmail.com>
2022-04-08 06:05:44 -0400

    trying to fix routing issues

--- matter/dashboard_header.php
+++ matter/dashboard_header.php
@@ -4,13 +4,13 @@
     <head>
         <title>McGill TA Management</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/dashboard.css">
+        <link rel="stylesheet" href="../css/dashboard.css">
     </head>
 
     <body>
 
         <!-- Landing Page Header -->
         <div class="dashboard-header">
-            <img class="dashboard-header-icon" src="images/icon-simple.svg">
-            <img class="dashboard-header-icon" id="settings-btn" src="images/settings-icon.svg">
+            <img class="dashboard-header-icon" src="../images/icon-simple.svg">
+            <img class="dashboard-header-icon" id="settings-btn" src="../images/settings-icon.svg">
         </div>
