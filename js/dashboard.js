08bb9841a44e5867793769cae2daf32043b53ab0
David Peterman <peterman.dd@gmail.com>
2022-04-08 17:42:37 -0400

    added event listener in dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,6 +8,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
         $('.dashboard-content-side-nav-bar.first-nav-bar').removeClass('open');
         $('.dashboard-content-side-nav-bar.second-nav-bar').addClass('open');
 
+        $(this).css({'background-color': ''})
+
         if ($(this).attr('id') == 'admin') {
             getSecondaryMenuItems('admin');
         }
@@ -21,6 +23,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
             fillSecondaryMenu('sys-ops');
         }
     });
+
+
 
 
     $('#main-dashboard').on('click', function() {
@@ -74,25 +78,30 @@ function fillSecondaryMenu(menuName) {
     if (menuName == 'sys-ops') {
         document.getElementById('second-nav-bar-options-container').innerHTML = 
         `
-        <div id="manage-users" class="nav-bar-btn-container">
+        <div id="manage-users" class="nav-bar-btn-container second-nav-bar">
             <div class="nav-bar-btn-wrapper  second-nav-bar">
                 <div class="nav-bar-btn">Manage Users</div>
             </div>
         </div>
-        <div id="import-users" class="nav-bar-btn-container">
+        <div id="import-users" class="nav-bar-btn-container second-nav-bar">
             <div class="nav-bar-btn-wrapper  second-nav-bar">
                 <div class="nav-bar-btn">Import</div>
             </div>
         </div>
-        <div id="add-manually-users" class="nav-bar-btn-container">
+        <div id="add-manually-users" class="nav-bar-btn-container second-nav-bar">
             <div class="nav-bar-btn-wrapper  second-nav-bar">
                 <div class="nav-bar-btn">Add Manually</div>
             </div>
         </div>
         `;
 
-
         // add event listeners that are specific to sys-ops menu
+
+        $('.nav-bar-btn-container.second-nav-bar').on('click', function() {
+            $('.nav-bar-btn-container.second-nav-bar').css({'color': 'rgb(103, 103, 103)'});
+            $(this).css({'color': '#7474ff'});
+        });
+
         $('#manage-users').on('click', function() {
             let user = document.getElementById('username').innerText;
             let syncRequest = new XMLHttpRequest();
