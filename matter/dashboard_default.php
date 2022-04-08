3a5b9a82de123f59efcaa7cf8eb5709dd3870ad7
David Peterman <peterman.dd@gmail.com>
2022-04-08 09:41:24 -0400

    minior bug fixes

--- matter/dashboard_default.php
+++ matter/dashboard_default.php
@@ -88,7 +88,7 @@
         
 
 
-        <div id="username"><?php echo $username ?></div>
+        <div id="username"><?php echo $_GET['user']?></div>
 
         <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5eab681659f730bdd5daac20" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="../js/dashboard.js"></script>
