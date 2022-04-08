2f289eb5d531e53b78a91d4b9c209ad07fe8994b
David Peterman <peterman.dd@gmail.com>
2022-04-08 03:14:14 -0400

    started working on register styles

---
+++ matter/dashboard_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>McGill TA Management</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/dashboard.css">
+    </head>
+
+    <body>
+
+        <!-- Landing Page Header -->
+        <div class="dashboard-header">
+            <img class="dashboard-header-icon" src="../images/icon-simple.svg">
+            <img class="dashboard-header-icon" id="settings-btn" src="../images/settings-icon.svg">
+        </div>
