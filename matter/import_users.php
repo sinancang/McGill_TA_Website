3a8172cd9b145e6a3d2dedc30ae0c6cfce5933ca
David Peterman <peterman.dd@gmail.com>
2022-04-09 12:45:31 -0400

    started creating UI for importing users through csv file upload

--- matter/import_users.php
+++ matter/import_users.php
@@ -1,1 +1,20 @@
-<div>import users</div>
+<div class="add-users-manually-form-container">
+
+    <div class="form-wrapper">
+
+        <div class="form-input-container">
+            <label for="file">Upload file</label>
+            <input type="file" name="file" id="file" required>
+        </div>
+
+
+
+        <button id="add-btn" type="button" onclick="">Add</button>
+
+    </div>
+
+    <div id="form-server-response-container">
+
+    </div>
+
+</div>
