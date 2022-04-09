59afda35c5937b9e899cc8a2cac0929ac7827dd0
David Peterman <peterman.dd@gmail.com>
2022-04-08 23:09:49 -0400

    added form to input_users_manually page

--- matter/add_users_manually.php
+++ matter/add_users_manually.php
@@ -1,1 +1,21 @@
-<div>add users manually</div>
+<div class="add-users-manually-form-container">
+
+    <form action="" method="post" class="form-example">
+
+        <div class="form-example">
+            <label for="name">Professor Name </label>
+            <input type="text" name="name" id="name" required>
+        </div>
+
+        <div class="form-example">
+            <label for="course-code">Course code</label>
+            <input type="text" name="course-code" required>
+        </div>
+
+        <div class="form-example">
+            <input type="submit" value="Add">
+        </div>
+
+    </form>
+
+</div>
