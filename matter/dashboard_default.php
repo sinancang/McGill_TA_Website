e8397da0cf840bb95135da54d059074bce5df570
David Peterman <peterman.dd@gmail.com>
2022-04-09 02:36:55 -0400

    small route bug fixed

--- matter/dashboard_default.php
+++ matter/dashboard_default.php
@@ -5,7 +5,7 @@
         <title>McGill TA Management</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="../css/dashboard.css">
-        <link real="stylesheet" href="../manual_input.css">
+        <link real="stylesheet" href="../css/manual_input.css">
     </head>
 
     <body>
