5d780886d2a88f9a70ebf4b341ef9fbdd3c35840
David Peterman <peterman.dd@gmail.com>
2022-04-09 22:27:48 -0400

    activity history is now dynamically generated content

--- matter/main_dashboard.php
+++ matter/main_dashboard.php
@@ -5,41 +5,23 @@
 
     <div class="recent-activity-title">RECENT ACTIVITY</div>
     <div class="recent-activity">
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
-        <div class="recent-activity-entry">
-            <div class="recent-activity-entry-action">Added TA David Peterman to course COMP 521</div>
-            <div class="recent-activity-entry-date">April 22nd</div>
-        </div>
+        
+        <?php 
+           
+            $filename = "../db/activity_history.json";
+            $data = file_get_contents($filename);
+            $history_data = json_decode($data, true);
+
+            $i = 0;
+            while (isset($history_data[$_GET['user']][$i])) {
+                $action = $history_data[$_GET['user']][$i]['action'];
+                $date = $history_data[$_GET['user']][$i]['date'];
+                echo "<div class='recent-activity-entry'>";
+                echo "<div class='recent-activity-entry-action'>{$action}</div>";
+                echo "<div class='recent-activity-entry-date'>{$date}</div>";
+                echo "</div>";
+            }
+        
+        ?>
     </div>
 </div>
