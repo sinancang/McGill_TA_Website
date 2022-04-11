bfa858d5aeb7df98e44c690de152ca2d5b5333c9
Sinan Can GULAN <sgulan@teaching.CS.McGill.CA>
2022-04-11 12:38:57 -0400

    added encryption to login.js

--- js/login.js
+++ js/login.js
@@ -1,19 +1,25 @@
 function sendLoginRequest(){ 
-	/*
+	
         var password = document.getElementById('pass').value;
 	let encryptRequest = new XMLHttpRequest();
 	var url1 = "../utils/encrypt.php";
 	encryptRequest.open("GET", url1, false);
-	encryptRequest.addEventListener("load",
-	*/
+	encryptRequest.addEventListener("load", function() {
+		logUserIn(username, this.responseText);
+	}, false);
+	var fd = new FormData;
+	fd.append('pass', password);
+	encryptRequest.send(fd);
 
+}
+function logUserIn(username, encrypted_password){
         // make Ajax call to login.php with username & encrypted password
         let syncRequest = new XMLHttpRequest();
         var url = "../routes/login.php";
         syncRequest.open("POST", url, false);
 
         syncRequest.addEventListener("load", function(){
-		console.log(this.status);
+                console.log(this.status);
                 if (this.status === 200) {
                         console.log(this.responseText);
                         window.location.href = `https://www.cs.mcgill.ca/~dpeter19/McGill_TA_Website/routes/dashboard.php?user=${this.responseText}&view=default`;
@@ -25,7 +31,7 @@ function sendLoginRequest(){
         }, false);
 
         var fd = new FormData;
-        fd.append('user', document.getElementById("user").value);
-        fd.append('pass', document.getElementById("pass").value);
-        syncRequest.send(fd);
+        fd.append('user', username);
+        fd.append('pass', encrypted_password);
+        syncRequest.send(fd);	
 }
