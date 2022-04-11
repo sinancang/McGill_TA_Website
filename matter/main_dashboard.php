cccd0b51b6b905054ca42d69e362cbaa953a1cf9
David Peterman <peterman.dd@gmail.com>
2022-04-11 15:13:29 -0400

    fixed small isuee in main_dashboard.php that cause infinite loop

--- matter/main_dashboard.php
+++ matter/main_dashboard.php
@@ -14,24 +14,25 @@
 
             // loop through avtivity history for this user and display in main dashboard
             $i = count($history_data[$_GET['user']]);
-            while ($i >= 0) {
-
-                $action = $history_data[$_GET['user']][$i]['action'];
-                $date = $history_data[$_GET['user']][$i]['date'];
-                echo "<div class='recent-activity-entry'>";
-                echo "<div class='recent-activity-entry-action'>{$action}</div>";
-                echo "<div class='recent-activity-entry-date'>{$date}</div>";
-                echo "</div>";
-                $i++;
-
-            }
-
             // if no activity history
             if ($i == 0) {
                 echo "<div class='recent-activity-entry'>";
                 echo "<div class='recent-activity-entry-action'>No activity to show</div>";
                 echo "</div>";
-            }       
+            } 
+            else {
+                while ($i >= 0) {
+
+                    $action = $history_data[$_GET['user']][$i]['action'];
+                    $date = $history_data[$_GET['user']][$i]['date'];
+                    echo "<div class='recent-activity-entry'>";
+                    echo "<div class='recent-activity-entry-action'>{$action}</div>";
+                    echo "<div class='recent-activity-entry-date'>{$date}</div>";
+                    echo "</div>";
+                    $i--;
+    
+                }
+            }
         ?>
     </div>
 </div>
