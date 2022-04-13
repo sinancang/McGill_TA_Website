296e82488100fa6640e46af38eaab8d2edbbf683
David Peterman <peterman.dd@gmail.com>
2022-04-13 19:24:18 -0400

    Started building manage users page

--- matter/manage_users.php
+++ matter/manage_users.php
@@ -1,12 +1,14 @@
 <div class="ajax-content-container">
 
-    <label for="user-type-select">Select User Type</label>
+    <div class="manage-users-container">
+        <label for="user-type-select">Select User Type</label>
 
-    <select name="user-types" id="user-type-select">
-        <option value="">Please choose an option</option>
-        <option value="ta">TA</option>
-        <option value="prof">Professor</option>
-        <option value="admin">Administrator</option>
-    </select>
+        <select name="user-types" id="user-type-select">
+            <option value="">Please choose an option</option>
+            <option value="ta">TA</option>
+            <option value="prof">Professor</option>
+            <option value="admin">Administrator</option>
+        </select>
+    </div>
 
 </div>
