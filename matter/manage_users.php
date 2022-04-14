71a6fe44b13e3924ea81148c94f348b27236df9d
David Peterman <peterman.dd@gmail.com>
2022-04-14 11:07:06 -0400

    UI update for managing users

--- matter/manage_users.php
+++ matter/manage_users.php
@@ -14,54 +14,70 @@
     -->
 
 
+    <div class="all-user-types-container">
 
-    <div class="user-accounts ta">
+        <div class="user-accounts ta">
 
-        <div class="user-account-type-title">TA USER ACCOUNTS</div>
+            <div class="user-account-type-title">TA USER ACCOUNTS</div>
 
-        <div class="user-account-entries">
+            <div class="user-account-entries">
 
-            <?php 
+                <?php 
 
-                $filename = "../db/user_by_role.json";
-                $data = file_get_contents($filename);
-                $user_data = json_decode($data, true);
+                    $filename = "../db/user_by_role.json";
+                    $data = file_get_contents($filename);
+                    $user_data = json_decode($data, true);
 
-                $TAs = $user_data['ta'];
-                $profs = $user_data['prof'];
-                $admins = $user_data['admin'];
+                    $TAs = $user_data['ta'];
+                    $profs = $user_data['prof'];
+                    $admins = $user_data['admin'];
+                    $students = $user_data['student'];
 
-                for ($i = 0; $i < count($TAs); $i++) {
-                    echo "<div class='user-account-entry'>{$TAs[$i]}</div>";
-                }
+                    for ($i = 0; $i < count($TAs); $i++) {
+                        echo "<div class='user-account-entry'>{$TAs[$i]}</div>";
+                    }
 
-            ?>
+                ?>
+            </div>
+
         </div>
+        <div class="user-accounts prof">
 
-    </div>
-    <div class="user-accounts prof">
+            <div class="user-account-type-title">PROFESSOR USER ACCOUNTS</div>
+            <div class="user-account-entries">
 
-        <div class="user-account-type-title">PROFESSOR USER ACCOUNTS</div>
-        <div class="user-account-entries">
+                <?php 
+                    for ($i = 0; $i < count($profs); $i++) {
+                        echo "<div class='user-account-entry'>{$profs[$i]}</div>";
+                    }
+                ?>
+            </div>
 
-            <?php 
-                for ($i = 0; $i < count($profs); $i++) {
-                    echo "<div class='user-account-entry'>{$profs[$i]}</div>";
-                }
-            ?>
         </div>
+        <div class="user-accounts admin">
 
-    </div>
-    <div class="user-accounts admin">
-
-        <div class="user-account-type-title">ADMIN USER ACCOUNTS</div>
-        <div class="user-account-entries">
-            <?php 
-                for ($i = 0; $i < count($admins); $i++) {
-                    echo "<div class='user-account-entry'>{$admins[$i]}</div>";
-                }
-            ?>
+            <div class="user-account-type-title">ADMIN USER ACCOUNTS</div>
+            <div class="user-account-entries">
+                <?php 
+                    for ($i = 0; $i < count($admins); $i++) {
+                        echo "<div class='user-account-entry'>{$admins[$i]}</div>";
+                    }
+                ?>
+            </div>
         </div>
+        <div class="user-accounts admin">
+
+            <div class="user-account-type-title">ADMIN USER ACCOUNTS</div>
+            <div class="user-account-entries">
+                <?php 
+                    for ($i = 0; $i < count($students); $i++) {
+                        echo "<div class='user-account-entry'>{$students[$i]}</div>";
+                    }
+                ?>
+            </div>
+
+        </div>
+        
     </div>
 
 </div>
