4143b2cdde1f9b85d44cb68a3340f19c2b0a6ff1
alaraalara <97011464+alaraalara@users.noreply.github.com>
2022-04-17 00:18:42 -0400

    Add files via upload

--- utils/rateTASubmission.php
+++ utils/rateTASubmission.php
@@ -3,7 +3,7 @@
 //creates json object from the html form. 
 
 $TAname =  $_POST['TA_dropdown'];
-$course = $_POST['course'];
+$course = $_POST['course_selected']; //the naming true??
 $term = $_POST['term'];
 $score = $_POST['score'];
 $review = $_POST['review'];
@@ -25,7 +25,7 @@ $post_data = array(
 $TAdata[] = $post_data;
 $final_data= json_encode($TAdata);
 
-if(file_put_contents('TA_review.json', $final_data)){
+if(file_put_contents("../db/TA_review.json", $final_data)){
     echo "appended";
 }
 else{
