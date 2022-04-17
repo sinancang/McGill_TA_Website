0168fd26300a1413e38e7fd9d76ee9316ffbacb9
alaraalara <97011464+alaraalara@users.noreply.github.com>
2022-04-17 00:49:12 -0400

    Add files via upload

--- routes/set_office_hours.php
+++ routes/set_office_hours.php
@@ -6,9 +6,13 @@
 
 $file = fopen("../db/office_hours.csv", "a+") or die("unable to open file!");
 
-$username="SomeUser1";
+//$username="SomeUser1";
 
-$userData = $username . "," .  $_GET['day'] . "," . $_GET['start'] . ", " . $_GET['end'] . "," . $_GET['location'] . "\n";
+$username=$_GET['username'];
+$coure=$_GET['course_selected'];
+echo $username;
+
+$userData = $username . "," .  $_GET['day'] . "," . $_GET['start'] . ", " . $_GET['end'] . "," .  $_GET['location'] . ",". $_GET['duties'] . ","  .  "\n";
 
 if(fwrite($file, $userData)){
 	echo "success";
