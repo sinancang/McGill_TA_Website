e954253039e3a3984eadb3717b53f372f561e276
David Peterman <peterman.dd@gmail.com>
2022-04-17 18:03:38 -0400

    logging in fix

--- utils/check_logged_in.php
+++ utils/check_logged_in.php
@@ -9,13 +9,13 @@
  * otherwise, http 404
  *
  */
-function check_logged_in(string $ticket_id, string $permission){
+function check_logged_in(string $ticket_id){
 	$file = fopen("../db/tickets.csv", "r") or die ("Unable to open file!");
 
 	$row = 1;
 	
 	while (($data = fgetcsv($file, 1000, ",")) !== FALSE){
-		if ($data[0] == $ticket_id && $data[1] == $permission){
+		if ($data[0] == $ticket_id){
 			return TRUE;
 		}
 	}	
