a7d95a82ea9d32efb261e193f68a3b209648c360
David Peterman <peterman.dd@gmail.com>
2022-04-16 23:47:00 -0400

    working on user editing

--- matter/manage_users.php
+++ matter/manage_users.php
@@ -131,6 +131,40 @@
                         }
 
                         echo "</div>";
+
+
+                        // edit user code. originally hidden
+                        echo
+                        "<div class='edit-user-info-container'>
+                            <div class='form-wrapper'>
+
+
+                                <div class='form-input-container'>
+                                    <label for='course-code'>Course code</label>
+                                    <input type='text' name='course-code' id='course-code' required>
+                                </div>
+                        
+                                <div class='form-input-container'>
+                                    <label for='course-name'>Course Name</label>
+                                    <input type='text' name='course-name' id='course-name' required>
+                                </div>
+                        
+                                <div class='form-input-container'>
+                                    <label for='term'>Term</label>
+                                    <input type='text' name='term' id='term' required>
+                                </div>
+                        
+                                <div class='form-input-container'>
+                                    <label for='new-prof'>Professor name</label>
+                                    <input type='text' name='new-prof' id='new-prof' required>
+                                </div>
+                
+                
+                
+                                <button class='submit-btn' id='add-btn' type='button' onclick='submitAddManuallyForm()'>Add</button>
+                
+                            </div>
+                        </div>"
                             
                     }    
 
