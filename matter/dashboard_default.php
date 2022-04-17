b6f1acb5eae113958ffd5fb4b11c4c1d4061c3ad
David Peterman <peterman.dd@gmail.com>
2022-04-16 23:33:17 -0400

    finished implementing adding new admin/prof

--- matter/dashboard_default.php
+++ matter/dashboard_default.php
@@ -159,31 +159,6 @@
                                 <button class="submit-btn" id="submit-add-new-user-btn" type="button">Add</button>
                                 <div class="new-user-server-response"></div>
                             </div>';
-                            
-                            echo
-                            '<div class="form-wrapper add-new-user-form">
-                                <div class="form-input-container">
-                                    <label for="name">Name</label>
-                                    <input type="text" name="name" id="new-user-name" required>
-                                </div>
-
-                                <div class="form-input-container">
-                                    <label for="email">Email</label>
-                                    <input type="text" name="email" id="new-user-email" required>
-                                </div>
-
-
-                                <div class="form-input-container">
-                                    <label for="type">User Type</label>
-                                    <select name="type" id="new-user-type">
-                                        <option value="admin">Admin</option>
-                                        <option value="prof">Professor</option>
-                                    </select>
-                                </div>
-
-                                <button class="submit-btn" id="submit-add-new-user-btn" type="button">Add</button>
-                                <div class="new-user-server-response"></div>
-                            </div>';
                         }
                     ?>
 
