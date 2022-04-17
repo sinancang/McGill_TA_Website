9c3432db0d617bdf7dbb7d96e16c5b5b83509d4d
Sinan Can GULAN <sgulan@teaching.CS.McGill.CA>
2022-04-17 16:40:38 -0400

    check login ticket

--- utils/check_logged_in.php
+++ utils/check_logged_in.php
@@ -11,11 +11,17 @@
  */
 function check_logged_in(string $ticket_id, string $permission){
 	$file = fopen("../db/tickets.csv", "r") or die ("Unable to open file!");
+
+	$row = 1;
+	
+	while (($data = fgetcsv($file, 1000, ",")) !== FALSE){
+		if ($data[0] == $ticket_id && $data[1] == $permission){
+			return TRUE;
+		}
+	}	
 	
 	
-	
-	
-	return true;
+	return false;
 }
 
 
