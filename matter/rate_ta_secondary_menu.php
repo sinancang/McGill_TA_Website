6e75c7530514b37dcf0d992f3f78a5676c7c728c
David Peterman <peterman.dd@gmail.com>
2022-04-17 14:50:23 -0400

    starting integrating ta rating

--- matter/rate_ta_secondary_menu.php
+++ matter/rate_ta_secondary_menu.php
@@ -20,5 +20,18 @@
             </div>";
         }
     }
+    else {
+        for ($i=0; $i<count($user_data['courses']); $i++) {
+            if ($user_data['courses'][$i]['role'] != 'ta') {
+                echo
+                "<div class='nav-bar-btn-container second-nav-bar'>
+                    <div class='nav-bar-btn-wrapper  second-nav-bar'>
+                            <div class='nav-bar-btn'>{$user_data['courses'][$i]['course num']}</div>
+                        </div>
+                    </div>
+                </div>";
+            }
+        }
+    }
 ?>
 
