7ad0d05201783bed3332add3a98ec1fce0ecda95
Sinan Can Gulan <57106474+sinancang@users.noreply.github.com>
2022-04-17 19:22:29 -0400

    Update check_logged_in.php

--- utils/check_logged_in.php
+++ utils/check_logged_in.php
@@ -4,17 +4,6 @@
  *
  */
 function check_logged_in(string $ticket_id, string $username){
-<<<<<<< HEAD
-	$filename = "../db/user_data.json";
-        $data = file_get_contents($filename);
-        $user_data = json_decode($data, true);
-
-	if (isset($user_data[$username]) && ($user_data[$username]['ticket'] == $ticket_id)){
-		return true;
-	}
-
-	return false;	
-=======
         $filename = "../db/user_data.json";
         $data = file_get_contents($filename);
         $user_data = json_decode($data, true);
@@ -24,7 +13,6 @@ function check_logged_in(string $ticket_id, string $username){
         }
 
         return false;
->>>>>>> e69944a26aa18b0cd43d1a49fd0e051ee9ddb949
 }
 
 
