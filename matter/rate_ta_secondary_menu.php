03f96936b2ff524e56695c30077fd33c423b61f6
David Peterman <peterman.dd@gmail.com>
2022-04-17 14:56:16 -0400

    starting integrating ta rating

--- matter/rate_ta_secondary_menu.php
+++ matter/rate_ta_secondary_menu.php
@@ -8,7 +8,6 @@
     $data2 = file_get_contents($filename2);
     $all_courses = json_decode($data2, true);
 
-    echo 'heeeeeeere';
 
     
     if ($user_data[$_GET['user']]['type'] == 'sysop') {
@@ -23,13 +22,12 @@
         }
     }
     else {
-        for ($i=0; $i<count($user_data['courses']); $i++) {
-            echo $user_data[$_GET['user']]['courses'][$i];
-            if ($user_data['courses'][$i]['role'] != 'ta') {
+        for ($i=0; $i<count($user_data[$_GET['user']]['courses']); $i++) {
+            if ($user_data[$_GET['user']]['courses'][$i]['role'] != 'ta') {
                 echo
                 "<div class='nav-bar-btn-container second-nav-bar'>
                     <div class='nav-bar-btn-wrapper  second-nav-bar'>
-                            <div class='nav-bar-btn'>{$user_data['courses'][$i]['course num']}</div>
+                            <div class='nav-bar-btn'>{$user_data[$_GET['user']]['courses'][$i]['course num']}</div>
                         </div>
                     </div>
                 </div>";
