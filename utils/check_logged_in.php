e69944a26aa18b0cd43d1a49fd0e051ee9ddb949
Sinan Can Gulan <57106474+sinancang@users.noreply.github.com>
2022-04-17 19:18:31 -0400

    Update check_logged_in.php

--- utils/check_logged_in.php
+++ utils/check_logged_in.php
@@ -2,26 +2,17 @@
 
 /* Checks if the user is logged in
  *
- * receives ticket id & permission
- * checks if ticket is in tickets.csv
- * if it is, checks if the ticket has the permission
- * if so, http 200
- * otherwise, http 404
- *
  */
-function check_logged_in(string $ticket_id, string $permission){
-	$file = fopen("../db/tickets.csv", "r") or die ("Unable to open file!");
+function check_logged_in(string $ticket_id, string $username){
+        $filename = "../db/user_data.json";
+        $data = file_get_contents($filename);
+        $user_data = json_decode($data, true);
 
-	$row = 1;
-	
-	while (($data = fgetcsv($file, 1000, ",")) !== FALSE){
-		if ($data[0] == $ticket_id && $data[1] == $permission){
-			return true;
-		}
-	}	
-	
-	
-	return false;
+        if (isset($user_data[$username]) && ($user_data[$username]['ticket'] == $ticket_id)){
+                return true;
+        }
+
+        return false;
 }
 
 
