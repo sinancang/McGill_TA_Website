ef05ee432de3fee61ec1b1ff2021babf327f27d8
David Peterman <peterman.dd@gmail.com>
2022-04-17 14:15:37 -0400

    starting integrating ta rating

--- matter/rate_ta_secondary_menu.php
+++ matter/rate_ta_secondary_menu.php
@@ -1,11 +1,11 @@
 
 <?php 
-
+    
     "<div class='nav-bar-btn-container second-nav-bar'>
-    <div class='nav-bar-btn-wrapper  second-nav-bar'>
-            <div class='nav-bar-btn'>Manage Users</div>
+        <div class='nav-bar-btn-wrapper  second-nav-bar'>
+                <div class='nav-bar-btn'>Manage Users</div>
+            </div>
         </div>
-    </div>
     </div>";
 
 ?>
