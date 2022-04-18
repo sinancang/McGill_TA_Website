0064f9b04192abeff25f3aec8e98d9596fcf9127
David Peterman <peterman.dd@gmail.com>
2022-04-17 22:52:52 -0400

    integrating rate ta

--- matter/rateTA.php
+++ matter/rateTA.php
@@ -8,16 +8,7 @@
                 <div id='form-course-term' style='font-size: 15px;'>{$_GET['course-term']}</div>";
             ?>
             <form method="post" name="form" style="padding-left: 5%;">
-                <!-- 
-                <div style="font-size: 15px;">Select a term:</div>
-                <select id = "term" name ="term" onchange="termSelected(this.value)"class="drop form-select"> 
-                    <option> -- Select a term --</option>
-                    <option> FALL 2020 </option>
-                    <option> WINTER 2021 </option>
-                    <option> FALL 2021 </option>
-                    <option> WINTER 2022 </option>
-                </select>
-                -->
+
                 <p style="font-size: 15px;">Select a TA from the options:</p>
                 <select class="drop form-select" id = "TA_dropdown" name="TA_dropdown">
                     <option value="">-- Select a TA --</option>
@@ -58,7 +49,7 @@
 
                 <p style="font-size: 15px;">Leave a review:</p>
                 <textarea id = "review" name="review" class="text-area" rows="5" style="width: 95%;"></textarea><br>
-                <button id="button" style="background-color: #d77171;" type="submit" onclick="sendCourseRequest()">Submit Review</button>  
+                <button id="button" style="background-color: #d77171;" onclick="sendCourseRequest()">Submit Review</button>  
             </form>
         </div>
     </div>
