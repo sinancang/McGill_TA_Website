c858a407000133d3057df95598813f8915791f25
David Peterman <peterman.dd@gmail.com>
2022-04-17 23:45:52 -0400

    integrating manage ta

--- js/dashboard.js
+++ js/dashboard.js
@@ -255,7 +255,16 @@ function fillSecondaryMenu(menuName) {
 function set_up_event_listeners_not_all_ta_report() {
     $('.ta-display-select').on('change', function() {
         if ($(this)[0].value == 'office-hours') {
-            
+            $('.display-option-ta-management').css({'display':'none'});
+            $('.display-option-ta-management.office-hours').css({'display':'flex'});
+        }
+        if ($(this)[0].value == 'performance-log') {
+            $('.display-option-ta-management').css({'display':'none'});
+            $('.display-option-ta-management.performance-log').css({'display':'flex'});
+        }
+        if ($(this)[0].value == 'ta-wishlist') {
+            $('.display-option-ta-management').css({'display':'none'});
+            $('.display-option-ta-management.ta-wishlist').css({'display':'flex'});
         }
     })
 }
