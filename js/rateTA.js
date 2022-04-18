4c475010ff3ca0604b81222845d659a1165e6b68
David Peterman <peterman.dd@gmail.com>
2022-04-17 22:54:42 -0400

    integrating rate ta

--- js/rateTA.js
+++ js/rateTA.js
@@ -24,33 +24,3 @@ function sendCourseRequest(){
         xhttp.send();
 }
 
-
-
-
-//When rating at TA, the user must specify which course and term.
-//Rating a TA gives a score from 0 to 5 (where 5 means the best). 
-//This option also provides a space to leave a short twitter- like comment.
-
-function sendRateTArequest(){
-    let course = document.getElementById('course').value;
-    let term = document.getElementById('term').value;
-    let score = document.getElementById('score').value;
-    let comment = document.getElementById('comment').value;
-    
-    syncRequest = new XMLHttpRequest();
-    let url = "../php/rateTA.php";
-    syncRequest.open("POST", url, true);
-
-    syncRequest.addEventListener("load", function(){
-        if (this.responseText == 'success') alert ('rating has been submitted');
-        else alert('failed to submit rating');
-        }, false);
-
-        let fd = new FormData;
-        fd.append ('course', username);
-        fd.append ('term', password);
-        fd.append('score', email);
-        fd.append('comment', studentid);
-        syncRequest.send (fd);
-
-}
