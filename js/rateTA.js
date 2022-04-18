03bdde7b78f60e1769f0a8cc931d9b3730a601f4
David Peterman <peterman.dd@gmail.com>
2022-04-17 22:29:13 -0400

    integrating rate ta

--- js/rateTA.js
+++ js/rateTA.js
@@ -9,15 +9,18 @@ function sendCourseRequest(){
         let review = document.getElementById('review').value;
         let rating = document.getElementById('score').value;
         let target_ta = document.getElementById('TA_dropdown').value;
-        
+
         var xhttp = new XMLHttpRequest();
         xhttp.open("GET", `../routes/dashboard.php?action=add-ta-review&ta-name=${target_ta}&rating=${rating}rating&review=${review}&course-term=${course_term}&course-code=${course_code}&user=${user}&ticket=${window.sessionStorage.ticket}`, true); //is this name true and can multiple variable be sent?
-        xhttp.onreadystatechange = function(){
-                if(xhttp.readyState==4 && xhttp.status==200){
-                        let response_div = `<div>${this.responseText}</div>`;
-                        $('.sign-up-form').append(response_div);
-                } 
+        xhttp.open("GET", url, true);  
+        xhttp.addEventListener("load", function(){           
+        if (this.status == 200) {
+                $('.dashboard-dynamic-content-main')[0].innerHTML = this.responseText;
         }
+        else alert('Server error. Please try again later');
+
+        }, false);
+
         xhttp.send();
 }
 
