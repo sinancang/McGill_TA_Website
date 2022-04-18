59f210b9b6870b2597e8ec11e7743719fc56a20f
David Peterman <peterman.dd@gmail.com>
2022-04-18 17:21:02 -0400

    integrating manage ta

--- utils/generateTA.php
+++ utils/generateTA.php
@@ -32,9 +32,9 @@ foreach($arr as $key=>$value){
 
 
 
-$file = fopen("../db/TA_notes.csv", "a+") or die("unable to open file!");
+$file = fopen("../db/TA_performance_logs.csv", "a+") or die("unable to open file!");
 
-$username=$_GET['username']; //not sure if working!
+$username=$_GET['username'];
 $course_selected=['course_selected'];
 
 //is review correct? Are commas protected?
