1bf1cdbcb0d02051a3e0513f8862411e73163ed4
alaraalara <97011464+alaraalara@users.noreply.github.com>
2022-04-19 17:52:55 -0400

    Add files via upload

--- utils/generateCourse.php
+++ utils/generateCourse.php
@@ -2,16 +2,17 @@
 $val = $_GET['course'];
 
 
-$filename = "../db/course_data.json";
+$filename = "../db/all_courses.json";
 $data = file_get_contents($filename);
 $arr = json_decode($data, true);
-$course = $_GET['course'];
+//$course = $_GET['course'];
 //$course = "Comp 307"; for debug
 echo "<option> Select a Course </option>";
 
 foreach($arr as $key=>$value){
-	//looping through each user 
-	echo "<option> $key </option>";
+	//looping through each user
+	$course =  $value['course code'];
+	echo "<option> $course </option>";
     //key has course in it
 
 }	
