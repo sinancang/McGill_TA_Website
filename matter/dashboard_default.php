5e20cfa5945153cb44ecf2d6ba15117c7c5b1e0b
David Peterman <peterman.dd@gmail.com>
2022-04-19 21:19:53 -0400

    integrating manage ta

--- matter/dashboard_default.php
+++ matter/dashboard_default.php
@@ -170,6 +170,7 @@
 
 
         <div id="username"><?php echo $_GET['user']?></div>
+        <div id="user-type"><?php echo $data[$username]['type']?></div>
         <div id="selected-course"></div>
 
         <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5eab681659f730bdd5daac20" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
